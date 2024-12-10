96a2baae8fe7cae8ac1d71cf7b83374d416e54c3
Pavan Wakade <130147767+pavanwakade@users.noreply.github.com>
2024-12-10 12:07:29 +0530

    Add files via upload

---
+++ java/conditional/Cnditional1.java
@@ -0,0 +1,11 @@
+class Cnditional1
+{
+		public static void main(String [] args)
+	{
+
+	   int a=200;
+	   int b=400;
+	   int d=a< b? b:a;
+	   System.out.println(d);
+	   }
+}
---
+++ java/conditional/Conditional1.java
@@ -0,0 +1,11 @@
+class Cnditional1
+{
+		public static void main(String [] args)
+	{
+
+	   int a=20;
+	   int b=40;
+	   int d=a<b ? a:b;
+	   System.out.println(d);
+	   }
+}
